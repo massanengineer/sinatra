ae21675f15ecceae28701b4c59177768142dd771
massanengineer <matsuda4645@gmail.com>
2023-06-12 02:10:05 +0900

    メモアプリ提出しました。

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # sinatra
+
+
+1. 作成したディレクトリでクローンを行う
+```
+$ git clone https://github.com/massanengineer/sinatra.git
+```
+
+2. gemをインストールする
+```
+$ bundle install
+```
+
+3. コマンドを実行する
+```
+$ ruby app.rb
+or
+$ bundle exec ruby app.rb
+```
---
+++ app.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+FILE_PATH = 'public/memos.json'
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+def get_memos(file_path)
+  File.open(file_path) do |file|
+    JSON.parse(file.read)
+  end
+end
+
+def set_memos(_file_path, memos)
+  File.open(FILE_PATH, 'w') do |file|
+    JSON.dump(memos, file)
+  end
+end
+
+get '/' do
+  redirect '/memos'
+end
+
+get '/memos' do
+  @memos = get_memos(FILE_PATH)
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+get '/memos/:id' do
+  memos = get_memos(FILE_PATH)
+  memo = memos[params[:id].to_s]
+  @title = memo['title']
+  @content = memo['content']
+  erb :show
+end
+
+post '/memos' do
+  title = params[:title]
+  content = params[:content]
+  memos = get_memos(FILE_PATH)
+  id = ((memos.keys.map(&:to_i).max || 0) + 1).to_s
+  memos[id] = { 'title' => title, 'content' => content }
+  set_memos(FILE_PATH, memos)
+  redirect '/memos'
+end
+
+get '/memos/:id/edit' do
+  memos = get_memos(FILE_PATH)
+  @title = memos[params[:id]]['title']
+  @content = memos[params[:id]]['content']
+  erb :edit
+end
+
+patch '/memos/:id' do
+  title = params[:title]
+  content = params[:content]
+  memos = get_memos(FILE_PATH)
+  memos[params[:id]] = { 'title' => title, 'content' => content }
+  set_memos(FILE_PATH, memos)
+  redirect "/memos/#{params[:id]}"
+end
+
+delete '/memos/:id' do
+  memos = get_memos(FILE_PATH)
+  memos.delete(params[:id])
+  set_memos(FILE_PATH, memos)
+  redirect '/memos'
+end
