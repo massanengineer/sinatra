034afa13d2764e520485a8b97bb885f67b022a9c
massanengineer <matsuda4645@gmail.com>
2023-06-26 18:52:33 +0900

    冗長部分削除

--- app.rb
+++ app.rb
@@ -10,14 +10,14 @@ helpers do
   end
 end
 
-CONNECTION = PG.connect(host: 'localhost', dbname: 'memo', user: '任意のユーザー', password: '任意のパスワード')
+CONNECTION = PG.connect(host: 'localhost', dbname: 'memo', user: 'postgres', password: 'matsuda4645')
 
 def post(title, content)
   CONNECTION.exec_params('INSERT INTO memos (title, content) VALUES ($1, $2)', [title, content])
 end
 
 def read(id)
-  result = CONNECTION.exec_params('SELECT * FROM memos WHERE id = $1', [id])
+  result = CONNECTION.exec('SELECT * FROM memos WHERE id = $1', [id])
   result.values.flatten
 end
 
